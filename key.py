51dfd7dc1b9af21c0f326e86d4aae3556bf7b923
dabadi9 <dabadi@bu.edu>
2020-02-04 19:45:00 -0500

    Create key.py file

---
+++ key.py
@@ -0,0 +1,4 @@
+consumer_key = 'your-consumer-key-here'
+consumer_secret = 'your-consumer-secret-here'
+access_token = 'your-access-token-here'
+access_token_secret = 'your-access-token-secret-here'
