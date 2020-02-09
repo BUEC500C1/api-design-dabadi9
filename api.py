10097b41f0d7da941e70ebcc5f3f106807b147da
dabadi9 <dabadi@bu.edu>
2020-02-09 15:29:16 -0500

    Add vision api

--- api.py
+++ api.py
@@ -1,26 +1,55 @@
 import tweepy
 import key
 import json
+from flask import Flask
+from flask_restful import Resource, Api
+from google.cloud import vision
+
+app = Flask(__name__)
+api = Api(app)
 
 
+def detect_labels_uri(uri):
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = uri
 
-def get_tweets():
-    auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
-    auth.set_access_token(key.access_token, key.access_token_secret)
-    tweepy_api = tweepy.API(auth)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
 
-    public_tweets = tweepy_api.home_timeline()
-    for tweet in public_tweets:
-        print(tweet.text)
-        if 'media' in tweet.entities:
-            print("Media:")
-            for media in tweet.entities["media"]:
-                print(media["media_url"])
-        print("")
-        print("________________________________________________________________________________________________________________________________________________________________________________________________")
-        print("")
-        print("")
+    response = {}
+    i = 0
 
+    for label in labels:
+        response[i] = label.description
+        i = i + 1
+
+    return response    
+
+
+class GetTweets(Resource):
+    def get(self):
+        auth = tweepy.OAuthHandler(key.consumer_key, key.consumer_secret)
+        auth.set_access_token(key.access_token, key.access_token_secret)
+        tweepy_api = tweepy.API(auth)
+
+        public_tweets = tweepy_api.home_timeline()
+
+        tweets = []
+
+        for tweet in public_tweets:
+            append_tweet = {'text': tweet.text}
+
+            if 'media' in tweet.entities:
+                append_tweet['media'] = []
+                for media in tweet.entities["media"]:
+                    append_tweet['media'].append(detect_labels_uri(media["media_url"]))
+            tweets.append(append_tweet)
+
+        return tweets
+
+
+api.add_resource(GetTweets, '/')
 
 if __name__ == "__main__":
-    get_tweets()
+    app.run(debug=True)
